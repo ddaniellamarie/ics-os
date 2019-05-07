f3becc9b9c2adea5c7e2abc8a0af21a81440a12b
ddaniellamarie <drcostales@up.edu.ph>
2019-05-07 13:07:58 +0800

    Homework 2

--- ics-os/kernel/console/console.c
+++ ics-os/kernel/console/console.c
@@ -872,6 +872,17 @@ int console_execute(const char *str){
          }
       }
    }else
+   if (strcmp(u,"add") == 0){ //-- Adds two integers. Args: <num1> <num2>
+      int a, b;
+      u = strtok(0," ");
+      a = atoi(u);
+      u = strtok(0," ");
+      b = atoi(u);
+      printf("%d + %d = %d\n",a,b,a+b);
+   }else
+   if (strcmp(u, "hello") == 0){
+      printf("Lala added this command. :)\n");
+   }else
    if (u[0] == '$'){                      //-- Sends message to a device.
       int i, devid;
       char devicename[255],*cmd;
